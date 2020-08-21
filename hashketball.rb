8320934e3c8f75e6af1c8ea3655192775cbdbb94
Sean Donohue <sdonohue0918@gmail.com>
2020-08-21 16:37:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,8 +205,9 @@ def big_shoe_rebounds
 
   game_hash.each do |vs_hash, team_values|
     team_values.each do |player_array, player_hash|
+      binding.pry
       player_hash.each do |player|
-        binding.pry
+        
       end
     end
   end
